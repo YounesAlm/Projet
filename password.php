fcd1c437b489ca472b02fde2c472bb0b5b89eda2
William Puzin <bill@MacBook-Pro-de-William.local>
2018-04-16 17:15:50 +0200

    password

---
+++ password.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Password</title>
+    </head>
+    <body>
+
+<?php
+    if (isset($_POST['identifiant']) AND isset($_POST['password']) AND $_POST['identifiant'] == "moi" AND $_POST['password'] == "mdp") {
+    	header('Location: menu.phtml');
+    } 
+
+    else{
+    	header('Location: page_acceuil.phtml');
+    }
+?>
+
+	</body>
+</html>
