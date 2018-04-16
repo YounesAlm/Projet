9af3a92db20b05f42bac354337d95bc6668899f0
William Puzin <bill@MacBook-Pro-de-William.local>
2018-04-16 22:24:31 +0200

    php page accueil 3

--- invite.php
+++ invite.php
@@ -1,10 +1,28 @@
-<?php
-// Ouverture fichier
-$invite = fopen('invite_mail.txt', 'r+')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Email_Visitor</title>
+    </head>
+    <body>
 
+		<?php
+			if(isset($_POST['email'])) {
+				$invite = fopen('invite_mail.txt', 'a');
+				$email = $_POST['email'];
+				$date = date("d-m-Y");
+				$heure = date("H:i");
 
+				fputs($invite, "\n".$email."\t".$date."\t".$heure."\n");
 
+				fclose($invite);
+				header('Location: database_visitor.phtml');
+			}
 
-// Fermeture fichier
-fclose($invite);
-?>
+			else{
+				header('Location: page_acceuil.html')
+			}
+		?>
+	
+	</body>
+</html>
--- password.php
+++ password.php
@@ -8,11 +8,11 @@
 
 <?php
     if (isset($_POST['identifiant']) AND isset($_POST['password']) AND $_POST['identifiant'] == "moi" AND $_POST['password'] == "mdp") {
-    	header('Location: menu.phtml');
+    	header('Location: menu.html');
     } 
 
     else{
-    	header('Location: page_acceuil2.phtml');
+    	header('Location: page_acceuil2.html');
     }
 ?>
 
